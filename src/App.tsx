0859cc83eff72e0dc7754b998655cff7572149f6
Aryan@123-rgb <srivatavaaryanalc76@gmail.com>
2024-02-13 15:45:53 +0530

    initialized the react project with vite and shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis magnam
+      reprehenderit beatae ea in recusandae, ipsa ipsum? Fugit accusantium earum
+      dolorem, laborum magni illum itaque nostrum, sunt quidem reprehenderit
+      quibusdam.
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
