e280cf58006602380568c4b7638db860c1f5127e
Aryan@123-rgb <srivatavaaryanalc76@gmail.com>
2024-02-26 20:54:33 +0530

    connected logout route with backend and added a dropdown profile menu for logged in users

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,3 @@
-import { Link, useNavigate } from "react-router-dom";
-import { Button } from "./ui/button";
-import { useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
@@ -9,20 +5,54 @@ import {
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
   DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { RootState } from "@/redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import { useGetUserDetailsQuery, useLogoutMutation } from "@/redux/slices/api";
+import { useEffect } from "react";
+import { updateCurrentUser, updateIsLoggedIn } from "@/redux/slices/userSlice";
+import { useToast } from "./ui/use-toast";
 
 function Header() {
   const { isLoggedIn, currentUser } = useSelector(
     (state: RootState) => state.userReducer
   );
+  const { data } = useGetUserDetailsQuery();
+  const [logout] = useLogoutMutation();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (data) {
+      dispatch(updateCurrentUser(data.data));
+      dispatch(updateIsLoggedIn(true));
+    }
+  }, [data]);
+
+  const handleLogout = async () => {
+    try {
+      const response = await logout().unwrap();
+      dispatch(updateIsLoggedIn(false));
+      navigate("/login");
+      toast({
+        title: response.message,
+        variant: "success",
+      });
+    } catch (error: any) {
+      toast({
+        title: error.data.message,
+        variant: "destructive",
+      });
+      console.log(error);
+    }
+  };
+
   return (
     <nav className="w-full h-[60px] bg-gray-900 text-white p-3 flex justify-between items-center">
       <Link to="/">
@@ -49,9 +79,7 @@ function Header() {
                 My Saved Codes
                 <DropdownMenuShortcut>&lt;&gt;</DropdownMenuShortcut>
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                Logout
-              </DropdownMenuItem>
+              <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
             </DropdownMenuGroup>
           </DropdownMenuContent>
         </DropdownMenu>
