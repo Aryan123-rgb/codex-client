68dfde972eec964cd1f71eed29e72092722278b6
Aryan@123-rgb <srivatavaaryanalc76@gmail.com>
2024-03-20 01:43:55 +0530

    env changed

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 
+
+
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
