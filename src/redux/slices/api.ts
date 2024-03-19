8c69826d803cffc4904eb4b019a4ca9061a8c14a
Aryan@123-rgb <srivatavaaryanalc76@gmail.com>
2024-03-19 22:50:21 +0530

    health check

--- src/redux/slices/api.ts
+++ src/redux/slices/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // import { CompilerSliceStateType } from "./compilerSlice";
-
+console.log(import.meta.env.VITE_BACKEND_BASE_URL)
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_BACKEND_BASE_URL,
